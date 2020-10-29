ff97d7d4da5891c85362838e050bf334050efb3a
Saiivy <gursimarhehar@gmail.com>
2020-10-29 17:14:52 -0400

    fixed everythin- fully functional

--- src/main/java/ca/sheridan/controllers/HomeController.java
+++ src/main/java/ca/sheridan/controllers/HomeController.java
@@ -18,8 +18,9 @@ public class HomeController {
 
 	/**
 	 * Using this we will redirect users to index page
-	 * @param model
-	 * @param mission
+	 * 
+	 * @param model: mission object for index page as model
+	 * @param mission : mission object
 	 * @return index
 	 */
 	@GetMapping("/")
@@ -27,40 +28,41 @@ public class HomeController {
 		model.addAttribute("mission", mission);
 		return "index";
 	}
-    
+
 	/*
-	 * A constructor for the database access
-	 * to be used here for making CRUD operations using 
-	 * user's data
+	 * A constructor for the database access to be used here for making CRUD
+	 * operations using user's data
 	 */
 	public HomeController(DatabaseAccess database) {
 		this.database = database;
 	}
 
-    /**
-     * Whenever user clicks on the link, he will be redirected to Createmission page,
-     * where he can add data to be added to database
-     * @param model
-     * @return create mision
-     */
+	/**
+	 * Whenever user clicks on the link, he will be redirected to Createmission
+	 * page, where he can add data to be added to database
+	 * 
+	 * @param model : mission as attribute
+	 * @return create mision
+	 */
 	@GetMapping("/createMission")
 	public String createMission(Model model) {
 		model.addAttribute("mission", new Mission());
-		return "createMission";
+		return "create_mission";
 	}
-    
+
 	/**
-	 * This method allows us to add mission to database 
-	 * calling the mission method using database constructor 
-	 * @param mission
+	 * This method allows us to add mission to database calling the mission method
+	 * using database constructor
+	 * 
+	 * @param mission: modelatttribute from create_mission
 	 * @param model
 	 * @return index page.
 	 */
 	@PostMapping("/addMission")
-	public String processForm(@ModelAttribute Mission mission, Model model) {
+	public String addMission(@ModelAttribute Mission mission, Model model) {
 		database.addMission(mission);
 		String message = "Mission Created Successfully";
-		model.addAttribute("message",message);
+		model.addAttribute("message", message);
 		return "index";
 
 	}
@@ -69,30 +71,52 @@ public class HomeController {
 	 * Created to get the name of the hero user wish to see missions for query the
 	 * database, if name matches missions are listed else user sees an error.
 	 * 
-	 * @param mission
-	 * @param hero
+	 * @param mission : mission object
+	 * @param hero : hero parameter from html page
 	 * @return viewMission page.
 	 */
 	@GetMapping("/viewMissions/")
-	public String viewMission(@RequestParam("hero") String hero,Model model) {   
+	public String viewMission(@RequestParam("hero") String hero, Model model) {
 		System.out.println(database.viewMission(hero));
-		model.addAttribute("missionList",database.viewMission(hero));
-		model.addAttribute("hero",hero);
-		return "viewMissions";
+		model.addAttribute("missionList", database.viewMission(hero));
+		model.addAttribute("hero", hero);
+		return "view_mission";
 
 	}
-	
+
 	/**
-	 * Invoking the deleteMission method using database constructor
-	 * which will delete the mission user desires using an unique ID.
-	 * @param id
+	 * Invoking the deleteMission method using database constructor which will
+	 * delete the mission user desires using an unique ID.
+	 * 
+	 * @param id : id of the mission
 	 * @return index
 	 */
 	@GetMapping("/delete/{id}")
 	public String deleteMission(@PathVariable Long id) {
 		database.deleteMission(id);
-			return "redirect:/";
-		        
+		return "redirect:/";
+
+	}
+	
+	/**
+	 * 
+	 * @param id: ID of the mission
+	 * @param model: mission object that we send as model to the View.
+	 * @return update_mission page
+	 */
+	@GetMapping("/edit/{id}")
+	public String getMission(@PathVariable Long id,Model model) {
+		Mission mission = database.getMission(id);
+		model.addAttribute("mission",mission);
+		return "update_mission";
+	}
+	
+	@PostMapping("/updateMission")
+	public String updateMission(@ModelAttribute Mission mission) {
+		database.updateMission(mission);
+		return "redirect:/"; 
 		
 	}
+	
+	
 }
